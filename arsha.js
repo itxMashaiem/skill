d702a3a918a66db7a273f0c1d1b92c6df293ee11
itxMashaiem <mmashaiemnadeemryk80@gmail.com>
2024-10-01 13:33:16 +0500

    Add files via upload

---
+++ arsha.js
@@ -0,0 +1,44 @@
+// function hideShow() {
+//     var x = document.getElementById("main");
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+//     } else {
+//         x.style.display = "none"
+//     }
+// }
+
+
+function video() {
+let 
+}
+
+
+
+
+
+function down() {
+    var x = document.getElementById("content");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none"
+    }
+}
+
+function down2() {
+    var x = document.getElementById("content1");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block"
+    }
+}
+
+function down3() {
+    var x = document.getElementById("content2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none"
+    }
+}
